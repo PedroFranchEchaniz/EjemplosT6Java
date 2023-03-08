95f2f00555a9bc5a3b27e733ba1f102ca7b1f91c
Pedro Franch <pedrofe.av@gmail.com>
2023-03-08 11:00:20 +0100

    excepciones

---
+++ src/excepciones/Principal.java
@@ -0,0 +1,21 @@
+package excepciones;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double numerador = 10, denominador = 1;
+		double resultado;
+		
+		
+		do {
+			
+			System.out.println("El denominador no puede ser 0");
+		}while (denominador == 0);
+		
+		resultado=numerador/denominador;
+		System.out.printf("%.2f" ,resultado);
+	}
+
+}
