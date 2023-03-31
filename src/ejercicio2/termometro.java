d7237579fd778bf2a436e68609ad495b41f4b10f
Pedro Franch <pedrofe.av@gmail.com>
2023-03-31 13:29:59 +0200

    cosas

--- src/ejercicio2/termometro.java
+++ src/ejercicio2/termometro.java
@@ -1,5 +1,27 @@
 package ejercicio2;
 
-public class termometro {
+public class termometro extends GestionError {
+	
+	private double temperatura;
+
+	public termometro(double temperatura) {
+		super();
+		this.temperatura = temperatura;
+	}
+
+	public double getTemperatura() {
+		return temperatura;
+	}
+
+	public void setTemperatura(double temperatura) {
+		this.temperatura = temperatura;
+	}
+
+	@Override
+	public String toString() {
+		return "termometro [temperatura=" + temperatura + "]";
+	}
+	
+	
 
 }
