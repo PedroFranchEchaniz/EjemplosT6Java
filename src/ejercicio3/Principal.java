0844e1d2120465b5c9ae9894ef5a9ea20f2c8acc
Pedro Franch <pedrofe.av@gmail.com>
2023-04-11 10:43:42 +0200

    bucle

--- src/ejercicio3/Principal.java
+++ src/ejercicio3/Principal.java
@@ -9,37 +9,40 @@ public class Principal {
 
 		Scanner sc = new Scanner(System.in);
 		CEcuacion2Grado ce = new CEcuacion2Grado();
-		
+
 		double a, b, c;
 		String aux;
-		
-		try {
-		System.out.println("Introduca el primer número");
-		aux=sc.nextLine();
-		a = Double.parseDouble(aux);
-		
-		System.out.println("Introduzca el segundo número");
-		aux=sc.nextLine();
-		b=Double.parseDouble(aux);
-		
-		System.out.println("Introduzca el tercer número");
-		aux=sc.nextLine();
-		c=Double.parseDouble(aux);
-		
-		System.out.println(ce.resolver1(a, b, c));
-		System.out.println(ce.resolver2(a, b, c));
-		
-		}catch(Error o) {
-			System.out.println(o.getMessage());			
-		}catch(Error2 e) {
-			System.out.println(e.getMessage());			
-		}catch(NumberFormatException i) {
-			System.out.println("No admite letras");
-		}
-		
-		
-		
-		
+		int respuesta = 0;
+
+		do {
+			try {
+				System.out.println("Introduca el primer número");
+				aux = sc.nextLine();
+				a = Double.parseDouble(aux);
+
+				System.out.println("Introduzca el segundo número");
+				aux = sc.nextLine();
+				b = Double.parseDouble(aux);
+
+				System.out.println("Introduzca el tercer número");
+				aux = sc.nextLine();
+				c = Double.parseDouble(aux);
+
+				System.out.println(ce.resolver1(a, b, c));
+				System.out.println(ce.resolver2(a, b, c));
+				
+			} catch (Error o) {
+				System.out.println(o.getMessage());
+			} catch (Error2 e) {
+				System.out.println(e.getMessage());
+			} catch (NumberFormatException i) {
+				System.out.println("No admite letras");
+			}
+			System.out.println("¿Desea continuar? -0 no -1 si");
+			aux = sc.nextLine();
+			respuesta = Integer.parseInt(aux);
+		} while (respuesta != 0);
+
 	}
 
 }
